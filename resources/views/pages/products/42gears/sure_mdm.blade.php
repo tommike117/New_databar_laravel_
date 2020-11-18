ede331237eb713305f8fbd77f8c183590f3907c7
Tommy <piyapat.t@databar.co.th>
2020-11-18 10:25:15 +0700

    update 42

--- resources/views/pages/products/42gears/sure_mdm.blade.php
+++ resources/views/pages/products/42gears/sure_mdm.blade.php
@@ -18,77 +18,25 @@ services')
         <picture>
             <img class="desktop-pic" src="/images/new-website/products/42gears/web/SureMDM/MDM_1920-01_r.jpg" width="100%">
             <img class="mobile-pic" src="/images/new-website/products/42gears/mobile/42_750-01.jpg" width="100%">
+            <a href="/contacts">
+                <button class="btn btn-banner">Contact now</button>
+            </a>
         </picture>
+        <div class="text-1">
+            <h2>
+                Overview
+            </h2>
+            <p>
+                SureMDM, powered by 42Gears UEM, is an intuitive and powerful Enterprise Mobility Management (EMM) tool for Android, iOS, Windows, Linux and macOS platforms. You can secure, monitor and manage company owned
+                devices for dedicated use as well as employee owned devices used to access company data (BYOD). SureMDM
+                 incorporates all aspects of enterprise mobility through unified endpoint management such as Mobile Application Management, Mobile Device Management and Mobile Content Management.
+            </p>
+        </div>
     </section>
     <section class="section-2">
 
     </section>
-    {{-- <section class="section-7">
-        <div class="row body-7">
-            <div class="col-lg-3">
-                <div class="box-7">
-                    <div class="text-7">
-                        <figure>
-                            <img src="/images/new-website/products/42gears/web/42_icon-01.png" width="45%" alt="">
-                        </figure>
-                        <h2>
-                            MOBILE DEVICE MANAGEMENT
-                        </h2>
-                        <p>
-                            Secure, monitor, and manage all your endpoints using 42Gears’ award-winning mobile device management platform.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="box-7">
-                    <div class="text-7">
-                        <figure>
-                            <img src="/images/new-website/products/42gears/web/42_icon-02.png" width="45%" alt="">
-                        </figure>
-                        <h2>
-                            RUGGED DEVICE MANAGEMENT
-                        </h2>
-                        <p>
-                            42Gears’ MDM solution brings the promise of data and device security to the table, even in harsh work environments.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="box-7">
-                    <div class="text-7">
-                        <figure>
-                            <img src="/images/new-website/products/42gears/web/42_icon-03.png" width="45%" alt="">
-                        </figure>
-                        <h2>
-                            VR DEVICE
-                            MANAGEMENT
-                        </h2>
-                        <p>
-                            42Gears’ VR device management solution eases VR adoption while ensuring data and device security.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="box-7">
-                    <div class="text-7">
-                        <figure>
-                            <img src="/images/new-website/products/42gears/web/42_icon-04.png" width="45%" alt="">
-                        </figure>
-                        <h2>
-                            THINGS
-                            MANAGEMENT
-                        </h2>
-                        <p>
-                            42Gears Things Management Technology makes it possible to manage and monitor devices that do not run any OS.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> --}}
+
 
 </div>
 <section>
